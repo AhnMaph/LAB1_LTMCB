be24e85e12d3a66b2c895976b42e4f56559ec734
Ahn <phamthanhanrule123@gmail.com>
2025-03-04 13:50:33 +0700

    create 5 forms and its buttons

--- LAB1_/MainForm1.Designer.cs
+++ LAB1_/MainForm1.Designer.cs
@@ -28,18 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
             SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(74, 138);
+            button1.Name = "button1";
+            button1.Size = new Size(135, 63);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += this.button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(316, 142);
+            button2.Name = "button2";
+            button2.Size = new Size(123, 59);
+            button2.TabIndex = 1;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += this.button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(536, 142);
+            button3.Name = "button3";
+            button3.Size = new Size(118, 59);
+            button3.TabIndex = 2;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += this.button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(144, 271);
+            button4.Name = "button4";
+            button4.Size = new Size(124, 72);
+            button4.TabIndex = 3;
+            button4.Text = "button4";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += this.button4_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(455, 280);
+            button5.Name = "button5";
+            button5.Size = new Size(119, 63);
+            button5.TabIndex = 4;
+            button5.Text = "button5";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += this.button5_Click;
             // 
             // MainForm1
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
             Name = "MainForm1";
             Text = "MainForm1";
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
     }
 }
