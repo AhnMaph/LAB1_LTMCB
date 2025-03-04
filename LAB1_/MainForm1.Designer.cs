bc44a98a3c6fb9f381d20d08a732f64e889a38f3
Ahn <phamthanhanrule123@gmail.com>
2025-03-04 15:24:05 +0700

    mainform redesign

--- LAB1_/MainForm1.Designer.cs
+++ LAB1_/MainForm1.Designer.cs
@@ -33,71 +33,125 @@
             button3 = new Button();
             button4 = new Button();
             button5 = new Button();
+            groupBox1 = new GroupBox();
+            label1 = new Label();
+            label2 = new Label();
+            groupBox2 = new GroupBox();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(74, 138);
+            button1.Location = new Point(54, 41);
             button1.Name = "button1";
             button1.Size = new Size(135, 63);
             button1.TabIndex = 0;
-            button1.Text = "button1";
+            button1.Text = "Bài 1";
             button1.UseVisualStyleBackColor = true;
-            button1.Click += this.button1_Click;
+            button1.Click += button1_Click;
             // 
             // button2
             // 
-            button2.Location = new Point(316, 142);
+            button2.Location = new Point(321, 45);
             button2.Name = "button2";
             button2.Size = new Size(123, 59);
             button2.TabIndex = 1;
-            button2.Text = "button2";
+            button2.Text = "Bài 2";
             button2.UseVisualStyleBackColor = true;
-            button2.Click += this.button2_Click;
+            button2.Click += button2_Click;
             // 
             // button3
             // 
-            button3.Location = new Point(536, 142);
+            button3.Location = new Point(567, 47);
             button3.Name = "button3";
-            button3.Size = new Size(118, 59);
+            button3.Size = new Size(118, 57);
             button3.TabIndex = 2;
-            button3.Text = "button3";
+            button3.Text = "Bài 3";
             button3.UseVisualStyleBackColor = true;
-            button3.Click += this.button3_Click;
+            button3.Click += button3_Click;
             // 
             // button4
             // 
-            button4.Location = new Point(144, 271);
+            button4.Location = new Point(185, 148);
             button4.Name = "button4";
-            button4.Size = new Size(124, 72);
+            button4.Size = new Size(134, 63);
             button4.TabIndex = 3;
-            button4.Text = "button4";
+            button4.Text = "Bài 4";
             button4.UseVisualStyleBackColor = true;
-            button4.Click += this.button4_Click;
+            button4.Click += button4_Click;
             // 
             // button5
             // 
-            button5.Location = new Point(455, 280);
+            button5.Location = new Point(445, 148);
             button5.Name = "button5";
-            button5.Size = new Size(119, 63);
+            button5.Size = new Size(127, 63);
             button5.TabIndex = 4;
-            button5.Text = "button5";
+            button5.Text = "Bài 5";
             button5.UseVisualStyleBackColor = true;
-            button5.Click += this.button5_Click;
+            button5.Click += button5_Click;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(button5);
+            groupBox1.Controls.Add(button4);
+            groupBox1.Controls.Add(button3);
+            groupBox1.Controls.Add(button2);
+            groupBox1.Controls.Add(button1);
+            groupBox1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            groupBox1.Location = new Point(30, 208);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(742, 230);
+            groupBox1.TabIndex = 5;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "List Bài Tập";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Times New Roman", 26F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(320, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(154, 60);
+            label1.TabIndex = 6;
+            label1.Text = "Lab 1";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI Semibold", 14F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(35, 37);
+            label2.Name = "label2";
+            label2.Size = new Size(404, 76);
+            label2.TabIndex = 7;
+            label2.Text = "1.Phạm Thanh An - 23520027\r\n2.Trần Trọng Nghĩa - 23521018\r\n";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(label2);
+            groupBox2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            groupBox2.Location = new Point(30, 72);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(742, 130);
+            groupBox2.TabIndex = 8;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Tên Thành Viên";
             // 
             // MainForm1
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(button5);
-            Controls.Add(button4);
-            Controls.Add(button3);
-            Controls.Add(button2);
-            Controls.Add(button1);
+            Controls.Add(groupBox2);
+            Controls.Add(label1);
+            Controls.Add(groupBox1);
             Name = "MainForm1";
             Text = "MainForm1";
+            groupBox1.ResumeLayout(false);
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -107,5 +161,9 @@
         private Button button3;
         private Button button4;
         private Button button5;
+        private GroupBox groupBox1;
+        private Label label1;
+        private Label label2;
+        private GroupBox groupBox2;
     }
 }
