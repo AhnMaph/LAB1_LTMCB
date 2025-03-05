524b648aa3a6bd010944b7d287c7adc110340472
Nghia2123 <23521018@gm.uit.edu.vn>
2025-03-05 17:18:52 +0700

    Bài 5 nearly finish

--- LAB1_/Form5.Designer.cs
+++ LAB1_/Form5.Designer.cs
@@ -34,30 +34,31 @@
             textBox1 = new TextBox();
             button1 = new Button();
             label3 = new Label();
-            label4 = new Label();
-            label5 = new Label();
-            label6 = new Label();
-            label7 = new Label();
-            label8 = new Label();
-            label9 = new Label();
+            diemTB = new Label();
+            diemMax = new Label();
+            monPass = new Label();
+            diemMin = new Label();
+            monFail = new Label();
+            rank = new Label();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Times New Roman", 26F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(242, 9);
+            label1.Location = new Point(315, 12);
+            label1.Margin = new Padding(4, 0, 4, 0);
             label1.Name = "label1";
-            label1.Size = new Size(353, 60);
+            label1.Size = new Size(477, 79);
             label1.TabIndex = 0;
             label1.Text = "Quản Lý Điểm";
             // 
             // listView1
             // 
-            listView1.Location = new Point(42, 205);
-            listView1.Margin = new Padding(3, 2, 3, 2);
+            listView1.Location = new Point(55, 262);
+            listView1.Margin = new Padding(4, 3, 4, 3);
             listView1.Name = "listView1";
-            listView1.Size = new Size(772, 355);
+            listView1.Size = new Size(1002, 453);
             listView1.TabIndex = 1;
             listView1.UseCompatibleStateImageBehavior = false;
             // 
@@ -65,27 +66,28 @@
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label2.Location = new Point(35, 114);
+            label2.Location = new Point(46, 146);
+            label2.Margin = new Padding(4, 0, 4, 0);
             label2.Name = "label2";
-            label2.Size = new Size(151, 26);
+            label2.Size = new Size(202, 36);
             label2.TabIndex = 2;
             label2.Text = "Dữ liệu điểm:";
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(192, 114);
-            textBox1.Margin = new Padding(3, 2, 3, 2);
+            textBox1.Location = new Point(250, 146);
+            textBox1.Margin = new Padding(4, 3, 4, 3);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(622, 31);
+            textBox1.Size = new Size(807, 39);
             textBox1.TabIndex = 3;
             // 
             // button1
             // 
             button1.Font = new Font("Times New Roman", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            button1.Location = new Point(681, 151);
-            button1.Margin = new Padding(3, 2, 3, 2);
+            button1.Location = new Point(885, 193);
+            button1.Margin = new Padding(4, 3, 4, 3);
             button1.Name = "button1";
-            button1.Size = new Size(133, 38);
+            button1.Size = new Size(173, 49);
             button1.TabIndex = 4;
             button1.Text = "Xuất";
             button1.UseVisualStyleBackColor = true;
@@ -96,90 +98,97 @@
             label3.AutoSize = true;
             label3.Font = new Font("Times New Roman", 10F, FontStyle.Italic, GraphicsUnit.Point, 0);
             label3.ForeColor = Color.Red;
-            label3.Location = new Point(42, 160);
+            label3.Location = new Point(55, 205);
+            label3.Margin = new Padding(4, 0, 4, 0);
             label3.Name = "label3";
-            label3.Size = new Size(492, 22);
+            label3.Size = new Size(683, 31);
             label3.TabIndex = 5;
             label3.Text = "*Lưu ý: Nhập với định dạng danh sách ví dụ: 9.5 8 7 6 5 10\r\n";
             // 
-            // label4
+            // diemTB
             // 
-            label4.AutoSize = true;
-            label4.Font = new Font("Times New Roman", 11F, FontStyle.Bold);
-            label4.Location = new Point(42, 572);
-            label4.Name = "label4";
-            label4.Size = new Size(105, 25);
-            label4.TabIndex = 6;
-            label4.Text = "Điểm TB:";
+            diemTB.AutoSize = true;
+            diemTB.Font = new Font("Times New Roman", 11F, FontStyle.Bold);
+            diemTB.Location = new Point(55, 732);
+            diemTB.Margin = new Padding(4, 0, 4, 0);
+            diemTB.Name = "diemTB";
+            diemTB.Size = new Size(140, 35);
+            diemTB.TabIndex = 6;
+            diemTB.Text = "Điểm TB:";
             // 
-            // label5
+            // diemMax
             // 
-            label5.AutoSize = true;
-            label5.Font = new Font("Times New Roman", 11F, FontStyle.Bold);
-            label5.Location = new Point(42, 617);
-            label5.Name = "label5";
-            label5.Size = new Size(155, 25);
-            label5.TabIndex = 7;
-            label5.Text = "Điểm cao nhất:";
+            diemMax.AutoSize = true;
+            diemMax.Font = new Font("Times New Roman", 11F, FontStyle.Bold);
+            diemMax.Location = new Point(55, 790);
+            diemMax.Margin = new Padding(4, 0, 4, 0);
+            diemMax.Name = "diemMax";
+            diemMax.Size = new Size(206, 35);
+            diemMax.TabIndex = 7;
+            diemMax.Text = "Điểm cao nhất:";
             // 
-            // label6
+            // monPass
             // 
-            label6.AutoSize = true;
-            label6.Font = new Font("Times New Roman", 11F, FontStyle.Bold);
-            label6.Location = new Point(305, 572);
-            label6.Name = "label6";
-            label6.Size = new Size(130, 25);
-            label6.TabIndex = 8;
-            label6.Text = "Số môn đậu:";
+            monPass.AutoSize = true;
+            monPass.Font = new Font("Times New Roman", 11F, FontStyle.Bold);
+            monPass.Location = new Point(396, 732);
+            monPass.Margin = new Padding(4, 0, 4, 0);
+            monPass.Name = "monPass";
+            monPass.Size = new Size(175, 35);
+            monPass.TabIndex = 8;
+            monPass.Text = "Số môn đậu:";
             // 
-            // label7
+            // diemMin
             // 
-            label7.AutoSize = true;
-            label7.Font = new Font("Times New Roman", 11F, FontStyle.Bold);
-            label7.Location = new Point(305, 617);
-            label7.Name = "label7";
-            label7.Size = new Size(165, 25);
-            label7.TabIndex = 9;
-            label7.Text = "Điểm thấp nhất:";
+            diemMin.AutoSize = true;
+            diemMin.Font = new Font("Times New Roman", 11F, FontStyle.Bold);
+            diemMin.Location = new Point(396, 790);
+            diemMin.Margin = new Padding(4, 0, 4, 0);
+            diemMin.Name = "diemMin";
+            diemMin.Size = new Size(221, 35);
+            diemMin.TabIndex = 9;
+            diemMin.Text = "Điểm thấp nhất:";
             // 
-            // label8
+            // monFail
             // 
-            label8.AutoSize = true;
-            label8.Font = new Font("Times New Roman", 11F, FontStyle.Bold);
-            label8.Location = new Point(579, 572);
-            label8.Name = "label8";
-            label8.Size = new Size(194, 25);
-            label8.TabIndex = 10;
-            label8.Text = "Số môn không đậu:";
+            monFail.AutoSize = true;
+            monFail.Font = new Font("Times New Roman", 11F, FontStyle.Bold);
+            monFail.Location = new Point(753, 732);
+            monFail.Margin = new Padding(4, 0, 4, 0);
+            monFail.Name = "monFail";
+            monFail.Size = new Size(260, 35);
+            monFail.TabIndex = 10;
+            monFail.Text = "Số môn không đậu:";
             // 
-            // label9
+            // rank
             // 
-            label9.AutoSize = true;
-            label9.Font = new Font("Times New Roman", 11F, FontStyle.Bold);
-            label9.Location = new Point(579, 617);
-            label9.Name = "label9";
-            label9.Size = new Size(96, 25);
-            label9.TabIndex = 11;
-            label9.Text = "Xếp loại:";
+            rank.AutoSize = true;
+            rank.Font = new Font("Times New Roman", 11F, FontStyle.Bold);
+            rank.Location = new Point(753, 790);
+            rank.Margin = new Padding(4, 0, 4, 0);
+            rank.Name = "rank";
+            rank.Size = new Size(129, 35);
+            rank.TabIndex = 11;
+            rank.Text = "Xếp loại:";
             // 
             // Form5
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(861, 660);
-            Controls.Add(label9);
-            Controls.Add(label8);
-            Controls.Add(label7);
-            Controls.Add(label6);
-            Controls.Add(label5);
-            Controls.Add(label4);
+            ClientSize = new Size(1119, 845);
+            Controls.Add(rank);
+            Controls.Add(monFail);
+            Controls.Add(diemMin);
+            Controls.Add(monPass);
+            Controls.Add(diemMax);
+            Controls.Add(diemTB);
             Controls.Add(label3);
             Controls.Add(button1);
             Controls.Add(textBox1);
             Controls.Add(label2);
             Controls.Add(listView1);
             Controls.Add(label1);
-            Margin = new Padding(3, 2, 3, 2);
+            Margin = new Padding(4, 3, 4, 3);
             Name = "Form5";
             Text = "Form5";
             ResumeLayout(false);
@@ -194,11 +203,11 @@
         private TextBox textBox1;
         private Button button1;
         private Label label3;
-        private Label label4;
-        private Label label5;
-        private Label label6;
-        private Label label7;
-        private Label label8;
-        private Label label9;
+        private Label diemTB;
+        private Label diemMax;
+        private Label monPass;
+        private Label diemMin;
+        private Label monFail;
+        private Label rank;
     }
 }
--- LAB1_/Form5.cs
+++ LAB1_/Form5.cs
@@ -21,8 +21,46 @@ namespace LAB1_
             return txt.Split(' ',StringSplitOptions.RemoveEmptyEntries)
                 .All(x => double.TryParse(x, out double num) && num > 0&& num<11);
         }
+
+        private string getRank(double avg, double[] mark) { 
+            // Count subjects which have mark < key in Dictionary
+            Dictionary<double, int> under_score = new Dictionary<double, int>();
+            under_score.Add(2, 0);
+            under_score.Add(3.5, 0);
+            under_score.Add(5, 0);
+            under_score.Add(6.5, 0);
+
+            foreach (double num in mark) {
+                foreach (double key in under_score.Keys) {
+                    if (num < key) {
+                        under_score[key]++;
+                    }
+                }
+            }
+
+            if (avg >= 8) {
+                if (under_score[6.5] == 0) return "Giỏi";
+            }
+
+            if (avg >= 6.5) {
+                if (under_score[5] == 0) return "Khá";
+            }
+
+            if (avg >= 5) {
+                if (under_score[3.5] == 0) return "Trung Bình";
+            }
+
+            if (avg >= 3.5) {
+                if (under_score[2] == 0) return "Yếu";
+            }
+
+            return "Kém";
+        }
         private void button1_Click(object sender, EventArgs e)
         {
+            // Clear list view every time button is clicked
+            listView1.Items.Clear();
+
             if (!check(textBox1.Text))
             {
                 MessageBox.Show("List điểm không hợp lệ");
@@ -30,18 +68,42 @@ namespace LAB1_
             }
             double[] arr = textBox1.Text.Split(' ', StringSplitOptions.RemoveEmptyEntries)
                 .Select(x => double.Parse(x)).ToArray();
-            
-            double avg = 0,min=11,max=0;
+
+            listView1.View = View.List;
+
+            for (int i = 0; i < arr.Length; i++) {
+                ListViewItem item = new ListViewItem("Môn " + (i + 1) + ": " + arr[i]);
+                Font itemFont = new Font("Times New Roman", 11, FontStyle.Bold);
+                item.Font = itemFont;
+                listView1.Items.Add(item);
+            }
+
+            double avg = 0;
+
+            Array.Sort(arr);
+
+            double min = arr[0];
+            double max = arr[arr.Length - 1];
+
             foreach (double num in arr) {
                 avg += num;
-                if (num < min) min = num;
-                if (num > max) max = num;
             }
-            
-            for (int i = 0;i<arr.Length;i++)
-            {
-                if(arr)
-            }    
+
+            avg /= arr.Length;
+
+            int subPass = 0, subFail = 0;            
+            for (int i = 0;i<arr.Length;i++) {
+                if (arr[i] >= 5) subPass++;
+                else subFail++;
+            }
+
+            diemTB.Text = "Điểm: " + Math.Round(avg, 2);
+            diemMax.Text = "Điểm cao nhất: " + max;
+            diemMin.Text = "Điểm thấp nhất: " + min;
+            monPass.Text = "Số môn đậu: " + subPass;
+            monFail.Text = "Số môn không đậu: " + subFail;
+            rank.Text = "Xếp loại: " + getRank(avg, arr);
+
         }
     }
 }
